26bc5aa9f9706a9f8d41bf64bf40f82c107bf3fd
dhydoalves <dhydoalves@gmail.com>
2026-03-21 13:54:24 -0300

    Fix: Estrutura de pacotes organizada e limpeza de target

--- src/main/java/br/com/erudio/PersonServices.java
+++ src/main/java/br/com/erudio/PersonServices.java
@@ -1,20 +1,27 @@
 package br.com.erudio;
 
-
 import br.com.erudio.model.Person;
 import org.springframework.stereotype.Service;
-
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
 @Service
 public class PersonServices {
 
-
     private final AtomicLong counter = new AtomicLong();
+    private Logger logger = Logger.getLogger(PersonServices.class.getName());
 
-    private Logger logger =
-            Logger.getLogger(PersonServices.class.getName());
+    // OOP: O método precisa receber um 'id' para saber QUEM buscar
+    public Person findById(String id) {
+        logger.info("Finding one person!");
 
-public Person findById()
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Josival");
+        person.setLastName("Alves");
+        person.setAddress("Campina Grande - Paraíba");
+        person.setGender("Male");
+
+        return person;
+    }
 }
